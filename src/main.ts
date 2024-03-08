b7fe3de57c69fc6f1ce578f82eb0bee612e5f766
inde153 <inde153@naver.com>
2024-03-08 20:20:03 +0900

    Feat : Cors & WebSocketAdapter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const PORT = process.env.PORT;
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+  //소켓 어뎁터로 연결 합니다.
+  app.useWebSocketAdapter(new IoAdapter(app));
+
   await app.listen(PORT);
 }
 bootstrap();
