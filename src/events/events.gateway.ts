04c2f27403c540ae80f381d060491682c309a90c
inde153 <inde153@naver.com>
2024-03-08 21:52:45 +0900

    Fix : WebSocket Setting

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,9 +1,34 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class EventsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway(8080, { cors: '*', namespace: 'user' })
+export class EventsGateway implements OnGatewayDisconnect, OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+  logger = new Logger();
+
+  async handleConnection(client: Socket): Promise<void> {
+    const socketId = client.id;
+    this.addClient(socketId);
+  }
+
+  @SubscribeMessage('send_message')
+  listenForMessages(@MessageBody() message: string) {
+    this.server.sockets.emit('receive_message', message);
+  }
+
+  addClient(socketId: string) {
+    throw new Error('Method not implemented.');
+  }
+  handleDisconnect(client: any) {
+    throw new Error('Method not implemented.');
   }
 }
